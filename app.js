405c1ff303c656b1ecb9c953ddb6c6d335c55358
Diogo Condêço <diogobcondeco@gmail.com>
2024-10-03 14:36:10 +0100

    routes added

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 
+// import todo router and user router
+const todoRouter = require("./routes/todo.routes");
+const userRouter = require("./routes/user.routes");
+
 // create express app
 const app = express();
 
 // add middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// middleware for API endpoints
+app.use("/api", userRouter);
+app.use("/api/todos", todoRouter);
 
 module.exports = app;
---
+++ routes/todo.routes.js
@@ -0,0 +1,23 @@
+// import express
+const express = require("express");
+
+// import Todo controller
+const todoController = require("./../controllers/todo.controller");
+
+// import authentication middleware
+const authController = require("./../auth/auth");
+
+// create router
+const router = express.Router();
+
+// API endpoint structure
+router.get("/", authController.authenticate, todoController.getAllPublicTodos);
+router.post("/", authController.authenticate, todoController.createATodo);
+router.put("/:todoId", authController.authenticate, todoController.updateATodo);
+router.delete(
+  "/:todoId",
+  authController.authenticate,
+  todoController.deleteATodo
+);
+
+module.exports = router;
